1315ce658b2d1f3fceaf4b1d89fc87d1271d774b
Duy Vo <85976363+DuyVo96@users.noreply.github.com>
2024-03-02 13:41:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min) + 1) + min;
+  return Math.floor(Math.random() * (max - min) + 50 - 10) + min;
 }
 
 module.exports = random;
